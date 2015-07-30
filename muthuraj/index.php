b8d6f67d61cb1c21482f144fc263ca1b922f81f8
muthuraj57 <muthu.sattur@gmail.com>
2015-07-30 22:03:33 +0530

    First Upload

---
+++ muthuraj/index.php
@@ -0,0 +1,263 @@
+
+
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Muthuaj | Student</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/jquery.scrolly.min.js"></script>
+		<script src="js/jquery.scrollzer.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-wide.css" />
+		</noscript>
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Header -->
+			<div id="header" class="skel-layers-fixed">
+
+				<div class="top">
+
+					<!-- Logo -->
+						<div id="logo">
+							<span class="image avatar48"><img src="images/avatar.jpg" alt="" /></span>
+							<h1 id="title">Muthuraj</h1>
+							<p>Engineering Student</p>
+						</div>
+
+					<!-- Nav -->
+						<nav id="nav">
+							<!--
+							
+								Prologue's nav expects links in one of two formats:
+								
+								1. Hash link (scrolls to a different section within the page)
+								
+								   <li><a href="#foobar" id="foobar-link" class="icon fa-whatever-icon-you-want skel-layers-ignoreHref"><span class="label">Foobar</span></a></li>
+
+								2. Standard link (sends the user to another page/site)
+
+								   <li><a href="http://foobar.tld" id="foobar-link" class="icon fa-whatever-icon-you-want"><span class="label">Foobar</span></a></li>
+							
+							-->
+							<ul>
+								<li><a href="#top" id="top-link" class="skel-layers-ignoreHref"><span class="icon fa-home">Intro</span></a></li>
+								<li><a href="#portfolio" id="portfolio-link" class="skel-layers-ignoreHref"><span class="icon fa-th">Portfolio</span></a></li>
+								<li><a href="#works" id="works-link" class="skel-layers-ignoreHref"><span class="icon fa-book">Works</span></a></li>
+								<li><a href="#about" id="about-link" class="skel-layers-ignoreHref"><span class="icon fa-user">About Me</span></a></li>
+								<li><a href="#contact" id="contact-link" class="skel-layers-ignoreHref"><span class="icon fa-envelope">Contact</span></a></li>
+							</ul>
+						</nav>
+						
+				</div>
+				
+				<div class="bottom">
+
+					<!-- Social Icons -->
+						<ul class="icons">
+							<li><a href="https://www.twitter.com/Muthu_Sattur" class="icon fa-twitter" target="_blank"><span class="label">Twitter</span></a></li>
+							<li><a href="https://www.facebook.com/muthuraj57" class="icon fa-facebook" target="_blank"><span class="label">Facebook</span></a></li>
+							<li><a href="https://plus.google.com/+MuthuRaj57" class="icon fa-github"  target="_blank"><span class="label">Github</span></a></li>
+							<!--<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>-->
+							<li><a href="mailto:muthu.sattur@gmail.com" class="icon fa-envelope"><span class="label">Email</span></a></li>
+						</ul>
+				
+				</div>
+			
+			</div>
+
+		<!-- Main -->
+			<div id="main">
+
+				<!-- Intro -->
+					<section id="top" class="one dark cover">
+						<div class="container">
+
+							<header>
+								<h2 class="alt">You need not to be diffeent. Just be you. That makes you different...</h2>
+								<p>This is my personal website to share my works, ideas and to tell about me. I'll share my works and thoughts and you do yours by creating your own website..</p>
+							</header>
+							
+							<footer>
+								<a href="#portfolio" class="button scrolly">Portfolio</a>
+							</footer>
+
+						</div>
+					</section>
+					
+				<!-- Portfolio -->
+					<section id="portfolio" class="two">
+						<div class="container">
+					
+							<header>
+								<h2>Portfolio</h2>
+							</header>
+							
+							<p></p>
+						
+							<div class="row">
+								<div class="4u">
+									<article class="item">
+										<a href="#" class="image fit"><img src="images/pic02.jpg" alt="" /></a>
+										<header>
+											<h3>Poster Presentation</h3>
+										</header>
+									</article>
+									<article class="item">
+										<a href="#" class="image fit"><img src="images/pic03.jpg" alt="" /></a>
+										<header>
+											<h3>Youth Present</h3>
+										</header>
+									</article>
+								</div>
+								<div class="4u">
+									<article class="item">
+										<a href="#" class="image fit"><img src="images/pic04.jpg" alt="" /></a>
+										<header>
+											<h3>Facebook Page</h3>
+										</header>
+									</article>
+									<article class="item">
+										<a href="#" class="image fit"><img src="images/pic05.jpg" alt="" /></a>
+										<header>
+											<h3>Gossip Website</h3>
+										</header>
+									</article>
+								</div>
+								<div class="4u">
+									<article class="item">
+										<a href="#" class="image fit"><img src="images/pic06.jpg" alt="" /></a>
+										<header>
+											<h3>Chat Website</h3>
+										</header>
+									</article>
+									<article class="item">
+										<a href="#" class="image fit"><img src="images/pic07.jpg" alt="" /></a>
+										<header>
+											<h3>My Personal Website</h3>
+										</header>
+									</article>
+								</div>
+							</div>
+
+						</div>
+					</section>
+
+					<!-- Works -->
+					<section id="works" class="five">
+						<div class="container">
+
+							<header>
+								<h2>Works</h2>
+							</header>
+
+							
+							
+							<p><h3>Blog</h3><ul><li><a data-target="true" data-title="Hack Tricks And Tech" data-type="url" data-url="http://hacktricksandtech.blogspot.com" href="http://hacktricksandtech.blogspot.com" target="_blank" title="Hack Tricks And Tech">hacktricksandtech.blogspot.com</a></li>
+	<li>I've created a blog to share my knowledge in technical side, some tricks and tweaks for android and PCs.</span></li>
+	<li>I created this blog with the help of Google's Blogger.</li>
+	<li>I mostly discussed about android, some features like rooting, it's pros and cons etc</li>
+</ul></p>
+
+							<p>
+								<h3>Chat Website</h3><ul><li><a data-target="true" data-type="url" data-url="http://www.gosssip.tk" href="http://www.gosssip.tk" target="_blank">www.gosssip.tk</a></li>
+	<li>This is my first website</li>
+	<li>I created this website using PHP, MySQL, HTML, AJAX, JavaScript.</li>
+	<li>Users should register in order to chat fromt this website. </li>
+	<li>Users can register through the register form in the website.</li>
+	<li>The registered users can do public as well as private chat</li>
+	<li>I also added a facility to check online users</li>
+	<li>I'll more and more features soon</li>
+</ul>
+							</p>
+							<p>
+								<h3>My Personal Portfolio</h3><ul><li><a data-target="true" data-type="url" data-url="http://www.muthuraj.tk" href="http://www.muthuraj.tk" target="_blank">www.muthuraj.tk</a></li>
+	<li>This is my second website :)</li>
+</ul>
+							</p>
+							
+							<p>
+								<h3>Android app </h3><ul>
+	<li>I'm developing an android app for gosssip.tk as an Instant Messaging app</li>
+	<li>It is in finishing conditon and I'll update with a link for the app when it is done</li>
+</ul>
+							</p>
+
+						</div>
+					</section>
+			
+				<!-- About Me -->
+					<section id="about" class="three">
+						<div class="container">
+
+							<header>
+								<h2>About Me</h2>
+							</header>
+
+							
+							
+							<p>I'm a student pursuing my B.E in Computer Science as major in Sethu Institute of Technology. I wish to learn more things as much as possible.</p>
+ 
+<p>Everyone wants to acheive big in their life. Yeah, me too wants to acheive bigger in my life as like everyone. So, we need to start somewhere to take steps to reach our aim. It does'nt matter where or how you start. </p>
+
+<p>I'll share my works and ideas through this site. Any feedbacks or ideas to improve this website or myself is warmly welcomed.</p>
+
+						</div>
+					</section>
+			
+				<!-- Contact -->
+					<section id="contact" class="four">
+						<div class="container">
+
+							<header>
+								<h2>Contact</h2>
+							</header>
+
+							<p>Feel free to contact me. You can contact me via social networking sites linked to the top of this page or you can submit it through this website.</p>
+							
+							<form method="post" action="#">
+								<div class="row 50%">
+									<div class="6u"><input type="text" name="name" placeholder="Name" /></div>
+									<div class="6u"><input type="text" name="email" placeholder="Email" /></div>
+								</div>
+								<div class="row 50%">
+									<div class="12u">
+										<textarea name="message" placeholder="Message"></textarea>
+									</div>
+								</div>
+								<div class="row">
+									<div class="12u">
+										<input type="submit" value="Send Message" />
+									</div>
+								</div>
+							</form>
+
+						</div>
+					</section>
+			
+			</div>
+
+		<!-- Footer -->
+			<div id="footer">
+				
+				<!-- Copyright -->
+					<ul class="copyright">
+						<li>&copy; muthuraj.tk. All rights reserved.</li>
+					</ul>
+				
+			</div>
+
+	</body>
+</html>
